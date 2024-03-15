eb4c17d609784117a9404f92e7d3d59898182899
Shivam Dinde <121174796+shivamdinde@users.noreply.github.com>
2024-03-15 12:07:14 +0530

    Add files via upload

---
+++ EduHUBAPIGateway/src/main/java/CDAC/EduHUBAPIGateway/EduHubapiGatewayApplication.java
@@ -0,0 +1,13 @@
+package CDAC.EduHUBAPIGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduHubapiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduHubapiGatewayApplication.class, args);
+	}
+
+}
---
+++ EduHUBAPIGateway/src/test/java/CDAC/EduHUBAPIGateway/EduHubapiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package CDAC.EduHUBAPIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduHubapiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
